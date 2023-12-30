a3beb511ae334b7c22528137910961b176097610
Prinz Adamz <prinzadamz@gmail.com>
2023-12-30 18:03:20 +0530

    Cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import Cart from './components/cart/Cart';
 import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: (<SignupPage/>),
+  },
+  {
+    path: "/cart",
+    element: (<Cart/>),
   },
 ]);
 
